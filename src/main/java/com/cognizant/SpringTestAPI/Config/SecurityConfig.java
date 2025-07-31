d9779de455791499245f22a45f6a4d8be29919e0
Prateek Mishra <prateek.mishra2@cognizant.com>
2025-07-31 13:24:22 +0530

    commit

--- src/main/java/com/cognizant/SpringTestAPI/Config/SecurityConfig.java
+++ src/main/java/com/cognizant/SpringTestAPI/Config/SecurityConfig.java
@@ -11,9 +11,6 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.lang.NonNull;
 import java.util.Arrays;
 
 
@@ -44,18 +41,5 @@ public class SecurityConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
-    }
-  
-@Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            public void addCorsMappings(@NonNull CorsRegistry registry) {
-                registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:4200")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("Content-Type", "Authorization", "x-xsrf-token")
-                        .allowedHeaders("*");
-            }
-        };
     }
 }
